9b15a15fca4976d618959a46a3433b330c0a6588
Swati Aggrawal <swatiagg357@gmail.com>
2022-06-11 10:55:59 +0530

    Add files via upload

---
+++ evenseries.c
@@ -0,0 +1,12 @@
+/* WAP to create an infinite even series */
+#include <stdlib.h>
+#include<stdio.h>
+int main(){
+    int difference=2;
+    int number=2;
+    while(2){
+    printf("\n difference: %d number: %d",difference,number);
+    number=number+difference;
+    }
+    return 0;
+}
