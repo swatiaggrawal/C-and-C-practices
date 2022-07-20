77dd83486a120da32ce903a6743be59a6c8e3ab4
Swati Aggrawal <swatiagg357@gmail.com>
2022-07-20 21:56:16 +0530

    Add files via upload

---
+++ comparestr.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    char str1[]="swat";
+    char str2[]="swati";
+    int l;
+    l=strcmp(str1,str2);
+    cout<<l;
+    return 0;
+}
